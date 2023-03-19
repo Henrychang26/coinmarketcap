3990c6ac6e2d88437e6a0e9ae1087e0b6a191178
Henrychang26 <henrychang26@gmail.com>
2023-03-19 02:56:51 -0700

    Coinmarketcap

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,29 @@ function App() {
   const [searchWord, setSearchWord] = useState<string>("");
 
   useEffect(() => {
-    setIsLoading(true);
-
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false"
-    )
-      .then((response) => response.json())
-      .then((data) => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+        );
+        const data = await response.json();
         setTokenData(data);
-        console.log(tokenData);
         setIsLoading(false);
-      })
-      .catch((error) => console.log(error));
-  }, []);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-  // useEffect(() => {
-  //   getData();
-  //   // const intervalId = setInterval(() => {
-  //   //   setCount((c) => c + 1); // ✅ Pass a state updater
-  //   // }, 1000);
-  //   // return () => clearInterval(intervalId);
-  // }, []); //✅ Now count is not a dependency
+    // Fetch data immediately on mount
+    fetchData();
+
+    // Fetch data every 10 seconds
+    const intervalId = setInterval(fetchData, 10000);
+
+    // Clean up the interval when the component unmounts or when the dependency array changes
+    return () => clearInterval(intervalId);
+  }, []);
 
   if (isLoading) {
     return <div>Loading</div>;
